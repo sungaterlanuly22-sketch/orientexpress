74059ec4c6c60415cdf7e68fc0fc12a8d631e487
Sungat <sungaterlanuly22@gmail.com>
2026-03-05 00:30:38 +0500

    fix main

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def contact():
     return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
