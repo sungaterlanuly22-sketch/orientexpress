0e1e6353a70ca90e276057fb8183d9ba0c6b34d5
Sungat <sungaterlanuly22@gmail.com>
2026-03-05 00:52:57 +0500

    update contact form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -26,6 +26,21 @@ def quality():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+# обработка формы
+@app.route("/send", methods=["POST"])
+def send():
+
+    name = request.form.get("name")
+    email = request.form.get("email")
+    message = request.form.get("message")
+
+    print("Имя:", name)
+    print("Email:", email)
+    print("Сообщение:", message)
+
+    return redirect("/contact")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
